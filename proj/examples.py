f3750997f71f20b7d627b32c03748d8d3d1507e9
JOAO AUGUSTO TONIAL LANER <01035589001@senacrs.edu.br>
2026-06-03 13:24:09 -0300

    Save drafts

--- proj/examples.py
+++ proj/examples.py
@@ -1,15 +1,50 @@
 ################# Exercícios (1ª aula) #################
 
+
 def calcula_area_triangulo(base: float, altura: float):
     area = (base * altura) / 2
     print(f"A área do triângulo é: {area}")
 
+
 def converte_temperatura_fahrenreit(graus_celsius: float):
-    temperatura = (graus_celsius * (9/5)) + 32
+    temperatura = (graus_celsius * (9 / 5)) + 32
     print(f"A temperatura em fahrenreit é: {temperatura}")
+
 
 def converte_em_dolares(reais: float):
     dolares = reais / 5.04
     print(f"O valor em dólares é: {dolares}")
 
 
+################# Exercícios (2ª aula) #################
+
+
+def classificar_pop(idade: int) -> str:
+
+    if idade <= 12:
+        return "Criança"
+
+    if idade > 12 and idade < 18:
+        return "Adolescente"
+
+    if idade >= 18 and idade <= 60:
+        return "Adulto"
+
+    return "Idoso"
+
+
+def tipo_triangulo(lado1: float, lado2: float, lado3: float):
+    if (lado1 + lado2) > lado3 \
+        or (lado3 + lado2) > lado1 \
+            or (lado1 + lado3) > lado2:
+        
+        if lado1 == lado2 == lado3:
+            return "Equilátero"
+        if (lado1 != lado2 != lado3): 
+            return "Escaleno"
+        return "Isóceles"
+
+def aprovar_saque(saldo:float, valor_saque:float):
+    if valor_saque <= saldo and valor_saque % 10 == 0:
+        return True
+    return False
